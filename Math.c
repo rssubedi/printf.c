b10b4e24ae7df15f7d4503c770c44a0e2a66cfc3
Rajeep Subedi <rajeep.subedi@trojans.dsu.edu>
2017-05-14 10:15:01 -0500

    Add files via upload

---
+++ Math.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int x, y, z;
+
+	x = 50;
+	y = 10;
+	z = 2;
+  printf("\n");
+	printf("There are %d christmas trees\n", x/y-z);
+	printf("         *                 *                    *\n");
+	printf("        ***               ***                  ***\n");
+	printf("       *****             *****                *****\n");
+	printf("      *******           *******              *******\n");
+	printf("     *********         *********            *********\n");
+	printf("    ***********       ***********          ***********\n");
+	printf("   *************     *************        *************\n");
+	printf("       *****             *****                *****\n");
+	printf("        ***               ***                  *** \n");
+	printf("\n");
+	printf("         1                 2                    3\n");
+	printf("\n");
+}
